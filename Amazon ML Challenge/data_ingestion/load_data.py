c5a6c42af9a1d541a0c38b07c9c6f3f7b6691711
Tanmay Wani <97271195+TANMAY-WANI@users.noreply.github.com>
2024-09-15 12:17:54 +0530

    Some structural changes

--- Amazon ML Challenge/data_ingestion/load_data.py
+++ Amazon ML Challenge/data_ingestion/load_data.py
@@ -1,7 +1,24 @@
 # load_data.py
+import csv
+import sys
+import os
+
+# Add the parent directory to sys.path so Python can find 'utils'
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.helpers import download_images
+
 def load_data(file_path):
     """
     Function to load data from a given file path.
     """
+    images = []
+    with open(file_path,'r') as train_data:
+        reader = csv.reader(train_data)
+        for row in reader:
+            print(row)
     # TODO: Implement data loading logic
     pass
+
+
+load_data("../../dataset/train.csv")
